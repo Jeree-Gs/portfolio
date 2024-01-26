2a3f47f3d11dd1630f4901eeadefdd3d1f5c160b
Jeree-Gs <garabito.jeremy.786@gmail.com>
2024-01-26 09:00:55 -0400

    add Roboto 3d and Loader

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Carga
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        document.querySelector('.loop').style.display = 'none';
+        document.querySelector('.content').style.display = 'block';
+    }, 3000); // Ajusta el tiempo en milisegundos según sea necesario
+});
+
+// Toggle Navbar
 const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
